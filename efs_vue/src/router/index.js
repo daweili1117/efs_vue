88399e4cb4f9333f1f071dd1a385880f9ba1bc4d
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2021-04-11 07:56:19 -0500

    2

--- efs_vue/src/router/index.js
+++ efs_vue/src/router/index.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Auth from '@/components/Auth'
+import CustomerList from '@/components/CustomerList'
+import CustomerCreate from '@/components/CustomerCreate'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/customer-list',
+      name: 'CustomerList',
+      component: CustomerList
+    },
+    {
+      path: '/customer-list/:msg',
+      name: 'CustomerUpdatedList',
+      component: CustomerList
+    },
+    {
+      path: '/customer-create',
+      name: 'CustomerCreate',
+      component: CustomerCreate
+    },
+    {
+      path: '/customer-create/:pk',
+      name: 'CustomerUpdate',
+      component: CustomerCreate
+    },
+    {
+      path: '/auth',
+      name: 'Auth',
+      component: Auth
     }
   ]
 })
